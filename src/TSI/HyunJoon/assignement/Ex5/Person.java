c31390701c939e0ff116c71f722292e357081f85
HJoonY <yooh9942@gmail.com>
2022-03-19 17:18:11 +0200

    third

---
+++ src/TSI/HyunJoon/assignement/Ex5/Person.java
@@ -0,0 +1,24 @@
+package TSI.HyunJoon.assignement.Ex5;
+
+import TSI.HyunJoon.Main;
+
+/**
+ * Exercise 5
+ */
+public class Person {
+    String name;
+    int age;
+    public Person(String Name,int Age){
+        name = Name;
+        age = Age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Hyun ",3);
+        System.out.println(person.name);
+        System.out.println(person.age);
+
+
+    }
+
+}
